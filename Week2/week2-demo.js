b85803035c8eb639803cc25711a536d5a1074e09
aaronxu73 <aaron.xu.989@gmail.com>
2025-02-25 20:29:35 -0600

    pushing week2 code

--- Week2/week2-demo.js
+++ Week2/week2-demo.js
@@ -1,13 +1,15 @@
 // WEEK 2 DEMO: Values, Variables, Functions
 // Task: Write a function to calculate the area of a triangle
 
-
 // Write function here
-
+function calculateTriangleArea(base, height){
+    return 0.5 * base * height;
+}
 
 // Define some variables to test it here
-
+base1 = 4;
+height1 = 6;
 
 // Call your function here!
-
+console.log(calculateTriangleArea(base1, height1));
 
--- Week2/week2-practice.js
+++ Week2/week2-practice.js
@@ -1,3 +1,17 @@
 // WEEK 2 PRACTICE: 
 // Task: Write a function named convertFeetToInches that takes in a parameter named "length" that represents the length of an object in feet and returns the length converted to inches.
 // After writing the function, add code that calls the function with lengths of 1 foot, 3 feet, and 30 feet and outputs these lengths converted to inches to the console.
+function convertFeetToInches(length){
+    return length * 12;
+}
+
+length1 = 1;
+length2 = 3;
+length3 = 30;
+
+inches = convertFeetToInches(1)
+
+console.log("Inches per one foot " + convertFeetToInches(length1));
+console.log(convertFeetToInches(length2));
+console.log(`Inches per 30 feet: ${convertFeetToInches(length3)}`);
+
