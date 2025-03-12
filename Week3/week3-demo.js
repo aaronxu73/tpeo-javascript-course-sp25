b4f1c9093c1a998d72a5821dc1d3c1c0d7cc889a
aaronxu73 <aaron.xu.989@gmail.com>
2025-03-11 20:22:34 -0500

    wk3

--- Week3/week3-demo.js
+++ Week3/week3-demo.js
@@ -4,3 +4,31 @@ Task: write a function, walkToTreasure, that takes in x and y coordinates on a b
 and “walks” to the location of the treasure buried in the sand. 
 */
 
+function walkToTreasure(x, y){
+    var treasureX = 3;
+    var treasureY = 9;
+
+    while (x != treasureX){
+        if(x < treasureX){
+            console.log("Going right");
+            x++;
+        } else{
+            console.log("Going left");
+            x--;
+        }
+    }
+
+    while (y != treasureY){
+        if(y < treasureY){
+            console.log("Going up");
+            y++;
+        } else{
+            console.log("Going down");
+            y--;
+        }
+    }
+    console.log("Found Treasure!")
+}
+
+walkToTreasure(0,0);
+
